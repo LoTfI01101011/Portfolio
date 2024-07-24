87c5d7c805e29c6f6f23b5e881bf8477a3ec7b80
Lotfi <lotfi.kaddari.lp2@gmail.com>
2024-07-24 18:50:18 +0100

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    assetsDir: 'assets', // Ensure assets are placed in the 'assets' directory
-  },
+  base: '/PORTFOLIO/',
 })
